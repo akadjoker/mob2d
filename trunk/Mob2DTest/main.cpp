0f69730e896e31161a63c4b975dc0e03aac9e00f
0rrm1k <0rrm1k@b9043734-26d9-11df-ab0a-9f3f633ae91d>
2010-03-08 18:48:53 +0000

    Re-adding it again!

---
+++ trunk/Mob2DTest/main.cpp
@@ -0,0 +1,138 @@
+// #define WINDOWS // uncomment this to build on windows.
+#define LINUX // comment this out to build on windows.
+
+
+// The only include you need for the system
+#include "../libMob2D/src/Mob2D.h"
+#include <GL/glfw.h>
+
+#ifdef LINUX
+    #include <sys/times.h>
+#endif
+#ifdef WINDOWS
+    #include <windows.h>
+#endif
+
+using namespace m2d;
+
+#ifdef LINUX
+uint32_t GetTickCount(void)
+{
+    tms tm;
+    return times(&tm)*10;
+}
+#endif
+
+const int TICKS_PER_SECOND = 30; // This is how many times per second DARSNode::StepFrame() will be called.
+const int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
+const int MAX_FRAMESKIP = 5;
+
+#ifdef LINUX
+    uint32_t next_game_tick = GetTickCount();
+#endif
+#ifdef WINDOWS
+    DWORD next_game_tick = GetTickCount();
+#endif
+
+int loops;
+
+int main()
+{
+    int     _width, _height;
+    int     frame = 0;
+    bool    running = true;
+
+    glfwInit();
+
+    if(!glfwOpenWindow(800, 600, 0, 0, 0, 0, 0, 0, GLFW_WINDOW))
+    {
+        glfwTerminate();
+        return 0;
+    }
+    glfwSetWindowTitle("Mob2D Test Application");
+
+    //-----INITALIZE Mob2D AND LOAD A RESOURCE-----//
+	m2dInit(800, 600, 700, 500); // 800x600 window
+
+	// Add a sprite markup to the internal cache.
+	m2dAddResource("resources/image_def.xml");
+    m2dAddResource("resources/zero_def.xml");
+
+	m2dSetClearColor(0.0f, 0.0f, 0.0f);
+
+    // Create app nodes and assign it to the image resources stored by the system.
+    // This is just a quick and dirty demonstration. I need to make a nicer one.
+	M2DNode app_node  = m2dAddNode("Test_Image");
+	M2DNode app_node1 = m2dAddNode("Zero");
+	M2DNode app_node2 = m2dAddNode("Test_Image");
+	M2DNode app_node3 = m2dAddNode("Zero");
+    M2DNode app_node4 = m2dAddNode("Test_Image");
+
+    // The lower the layer number, the more things it will be drawn over.
+    // The value can be anywhere between -9001 and 9001
+    // Nodes default to layer 0.0f
+    //app_node3->SetLayer(-20.0f);
+
+    // Move the sprite relative to the top left.
+	app_node ->Translate(100, 300); // left side
+    app_node1->Translate(400, 100); // top side
+    app_node2->Translate(700, 300); // right side
+    app_node3->Translate(400, 500); // bottom side
+    app_node4->Translate(400, 300); // bottom. this should be bellow the zero image.*/
+
+    // The lower the layer, the more it is guaranteed to be rendered on top of other objects.
+    // app_node4->SetLayer(-1.0f);
+    app_node3->SetLayer(-1000.0f);
+
+	// If an animation is not set for a node, then it will not display.
+	// Unset nodes default to the "ERROR" animation. Same thing occurs when an animation string is invalid.
+	app_node->SetAnimation("letters");
+	app_node1->SetAnimation("Transport_Zero");
+	app_node2->SetAnimation("letters");
+	app_node3->SetAnimation("Transport_Zero");
+	app_node4->SetAnimation("letters");
+
+    // Dump the log to the console so we can see if anything went wrong.
+	m2dDumpLogConsole();
+
+	app_node4->SetFlag(M2D_DRAWTOSCREEN);
+
+    while(running)
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        frame++;
+
+        glfwGetWindowSize(&_width, &_height);
+
+        _height = _height > 0 ? _height : 1;
+
+        // Basic framerate limitation.
+        loops = 0;
+        while((GetTickCount() > next_game_tick) && (loops < MAX_FRAMESKIP))
+        {
+            app_node->StepFrame();
+            app_node1->StepFrame();
+            app_node2->StepFrame();
+            app_node3->StepFrame();
+            app_node4->StepFrame();
+
+            next_game_tick += SKIP_TICKS;
+            loops++;
+        }
+
+        //-----INSERT Mob2D RENDERING CODE HERE-----//
+		m2dFinalizeFrame(/*Interpolation will be implimented soon!*/);
+
+        glfwSwapBuffers();
+
+    // exit if ESC was pressed or window was closed
+        running = (!glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED));
+    }
+    // Mob2D::API()->DumpLogFile();
+    m2dDeinit();
+
+    glfwTerminate();
+
+    return 0;
+}
---
+++ trunk/libMob2D/src/Mob2D.cpp
@@ -0,0 +1,110 @@
+#include "Mob2D.h"
+/*
+Mob2D*
+Mob2D::m_pInstance = NULL;
+*/
+
+namespace Mob2D {
+
+#ifdef _cplusplus
+extern "C" {
+#endif
+
+Mob2DCoord m2dGetWorldCoords(int x, int y)
+{
+    GLint viewport[4];
+
+    GLfloat win_x, win_y, win_z;
+    GLdouble pos_z;
+    GLdouble modelview_mat[16];
+    GLdouble projection_mat[16];
+
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    glGetDoublev(GL_PROJECTION_MATRIX, projection_mat);
+    glGetDoublev(GL_MODELVIEW_MATRIX, modelview_mat);
+
+    win_x = (float)x;
+    win_y = (float)viewport[3] - (float)y;
+    glReadPixels(x, int(win_y), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &win_z);
+
+    Mob2DCoord coord;
+    gluUnProject(win_x, win_y, win_z, modelview_mat, projection_mat, viewport, &coord.x, &coord.y, &pos_z);
+    return coord;
+}
+void m2dInit(uint window_width, uint window_height, uint view_width, uint view_height)
+{
+    SpriteManager::Instance()->Init();
+	Mob2DRenderer::Instance()->Init(window_width, window_height, view_width, view_height);
+	Mob2DLog::Instance();
+}
+void m2dDeinit()
+{
+    SpriteManager::Instance()->Deinit();
+	Mob2DRenderer::Instance()->Deinit();
+}
+void m2dMoveCamera(int x, int y)
+{
+    Mob2DRenderer::Instance()->MoveCamera(x, y);
+}
+void m2dSetViewport(int width, int height)
+{
+    Mob2DRenderer::Instance()->SetView(width, height);
+}
+
+void m2dFinalizeFrame()
+{
+    Mob2DRenderer::Instance()->Render();
+}
+
+void m2dCleanup()
+{
+    SpriteManager::Instance()->Cleanup();
+}
+
+void m2dAddResource(string file)
+{
+    SpriteManager::Instance()->AddResource(file);
+}
+
+void m2dDeleteSprite(string handle)
+{
+    SpriteManager::Instance()->DeleteSprite(handle);
+}
+
+void m2dClearNodes(string handle)
+{
+    SpriteManager::Instance()->ClearNodes(handle);
+}
+
+M2DNode m2dAddNode(string handle)
+{
+    return SpriteManager::Instance()->AddNode(handle);
+}
+
+void m2dSetClearColor(float r, float g, float b)
+{
+    GLfloat _r = (GLfloat)r / 255,
+			_g = (GLfloat)g / 255,
+			_b = (GLfloat)b / 255;
+
+	if(_r > 1.0f) _r = 1.0f;
+	if(_g > 1.0f) _g = 1.0f;
+	if(_b > 1.0f) _b = 1.0f;
+
+    glClearColor(_r, _g, _b, 0.0f);
+}
+void m2dDumpLogConsole()
+{
+    Mob2DLog::Instance()->ConsoleDump();
+}
+void m2dDumpLogFile()
+{
+    Mob2DLog::Instance()->FileDump();
+}
+
+#ifdef _cplusplus
+}
+#endif
+
+} // m2d namespace
